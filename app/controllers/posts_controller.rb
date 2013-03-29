fff4c9058befb1adf609c97fcae504ddfaa976d1
Robert Mathews <rob@justsoftwareconsulting.com>
2013-03-29 10:40:17 -0400

    tested simple override, works fine

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
+  before_filter :new, :initialize_post
+  
+  def initialize_post
+    @post = Post.new(:name=>'Smashing kittens', :organization_id=>current_user.organization_id)
+  end
+  
   load_and_authorize_resource :post
   def index
   end
